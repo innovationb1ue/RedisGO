090a4af8dc5367c6b1f640e2b2a4f986868ab7d1
Jeff Blue <517262600@qq.com>
2022-10-18 16:37:18 +0800

    handle options in ZADD

--- memdb/sorted_set.go
+++ memdb/sorted_set.go
@@ -1,6 +1,7 @@
 package memdb
 
 import (
+	"fmt"
 	"github.com/innovationb1ue/RedisGO/resp"
 	"log"
 	"net"
@@ -19,7 +20,7 @@ func zadd(m *MemDb, cmd cmdBytes, _ net.Conn) resp.RedisData {
 	}
 	// phrase options
 	optionsStr := ""
-	// skip "ZADD" & "{KEY}"
+	// skip "ZADD" and "{KEY}"
 	for _, s := range elems[2:] {
 		optionsStr += s + "/"
 	}
@@ -37,11 +38,16 @@ func zadd(m *MemDb, cmd cmdBytes, _ net.Conn) resp.RedisData {
 	if (optionDict["gt"] && optionDict["lt"]) || (optionDict["nx"] && optionDict["gt"]) || (optionDict["nx"] && optionDict["lt"]) {
 		return resp.MakeErrorData("ERR GT, LT, and/or NX options at the same time are not compatible")
 	}
+	if optionDict["incr"] && len(cmd) != 5 {
+		return resp.MakeErrorData("ERR INCR option supports a single increment-element pair")
+	}
+
 	// lock the key
 	m.locks.Lock(key)
 	defer m.locks.UnLock(key)
+	// declare sortedSet data structure
 	var sortedSet *SortedSet[*SortedSetNode]
-	// get key, create a sorted list if key does not exist
+	// get key, create a new sorted list if the key does not exist
 	SortedsetTmp, ok := m.db.Get(key)
 	if !ok {
 		sortedSet = NewSortedSet()
@@ -52,7 +58,8 @@ func zadd(m *MemDb, cmd cmdBytes, _ net.Conn) resp.RedisData {
 			return resp.MakeWrongType()
 		}
 	}
-	var addedCount int64
+	var retInt int64
+	var targetScore float64 // used when incr option
 	// set value:member pairs
 	// todo: optimize performance here. dont create a single virtual node in each loop
 	for i := idx; i < len(cmd); i += 2 {
@@ -67,25 +74,45 @@ func zadd(m *MemDb, cmd cmdBytes, _ net.Conn) resp.RedisData {
 		}
 		// get member Names
 		member := string(cmd[i+1])
-		// insert value (temporal structure here)
-		r := &SortedSetNode{
-			Names: map[string]struct{}{member: {}},
-			Score: score,
+		// try to get the old member if exist
+		node := sortedSet.GetByName(member)
+		var r *SortedSetNode
+		// check incr option or normally set the score
+		if optionDict["incr"] && node != nil {
+			targetScore = node.Value.GetScore() + score
+			// create a virtual node to hold member names
+			r = &SortedSetNode{
+				Names: map[string]struct{}{member: {}},
+				Score: targetScore,
+			}
+		} else {
+			r = &SortedSetNode{
+				Names: map[string]struct{}{member: {}},
+				Score: score,
+			}
 		}
-		// decide member count
+		// decide the return int64 value
+		// ch means return the number of value changed	(added + changed)
+		// normally we only count the member added 		(added)
+		if optionDict["ch"] && node.Value.GetScore() != score {
+			retInt++
+		} else
+		// member non-exist
 		if _, ok := sortedSet.dict[member]; !ok {
-			addedCount++
+			retInt++
 		}
 		// if the member exists, delete it first
-		node := sortedSet.GetByName(member)
 		if node != nil {
 			sortedSet.Delete(member)
 		}
-		// insert member
+		// insert new member
 		sortedSet.Insert(r)
 	}
 	log.Println(sortedSet.Values())
-	return resp.MakeIntData(addedCount)
+	if optionDict["incr"] {
+		return resp.MakeBulkData(resp.MakePlainData(fmt.Sprintf("%f", targetScore)).ByteData())
+	}
+	return resp.MakeIntData(retInt)
 }
 
 func RegisterSortedSetCommands() {
