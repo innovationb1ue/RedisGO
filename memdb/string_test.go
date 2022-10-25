000ef5aacc8a87face53b3ad4e0a7087408e0e68
Jeff Blue <517262600@qq.com>
2022-10-25 17:00:02 +0800

    fix string test

--- memdb/string_test.go
+++ memdb/string_test.go
@@ -2,6 +2,7 @@ package memdb
 
 import (
 	"bytes"
+	"context"
 	"testing"
 	"time"
 
@@ -16,9 +17,9 @@ func init() {
 
 func TestSetString(t *testing.T) {
 	mem := NewMemDb()
-
+	ctx := context.Background()
 	// test set
-	res := setString(mem, [][]byte{[]byte("set"), []byte("a"), []byte("a")}, nil)
+	res := setString(ctx, mem, [][]byte{[]byte("set"), []byte("a"), []byte("a")}, nil)
 	if !bytes.Equal(res.ToBytes(), []byte("+OK\r\n")) {
 		t.Error("set reply error")
 	}
@@ -28,7 +29,7 @@ func TestSetString(t *testing.T) {
 	}
 
 	// test opt xx and ex
-	res = setString(mem, [][]byte{[]byte("set"), []byte("a"), []byte("b"), []byte("xx"), []byte("ex"), []byte("100")}, nil)
+	res = setString(ctx, mem, [][]byte{[]byte("set"), []byte("a"), []byte("b"), []byte("xx"), []byte("ex"), []byte("100")}, nil)
 	if !bytes.Equal(res.ToBytes(), []byte("+OK\r\n")) {
 		t.Error("set reply error")
 	}
@@ -37,17 +38,17 @@ func TestSetString(t *testing.T) {
 		t.Error("set value error")
 	}
 	ttl, ok := mem.ttlKeys.Get("a")
-	if !ok || ttl.(int64)-time.Now().Unix() > 100 || ttl.(int64)-time.Now().Unix() < 99 {
+	if !ok || ttl.(*TTLInfo).value-time.Now().Unix() > 100 || ttl.(*TTLInfo).value-time.Now().Unix() < 99 {
 		t.Error("set ttl error")
 	}
 
 	// test opt keepttl
-	res = setString(mem, [][]byte{[]byte("set"), []byte("a"), []byte("c"), []byte("get"), []byte("keepttl")}, nil)
+	res = setString(ctx, mem, [][]byte{[]byte("set"), []byte("a"), []byte("c"), []byte("get"), []byte("keepttl")}, nil)
 	if !bytes.Equal(res.ToBytes(), []byte("$1\r\nb\r\n")) {
 		t.Error("set reply error")
 	}
 	_, ok = mem.ttlKeys.Get("a")
-	if ok {
+	if !ok {
 		t.Error("set keepttl error")
 	}
 }
