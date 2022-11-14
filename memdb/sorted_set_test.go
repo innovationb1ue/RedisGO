58a37e7972b8e70788d8545942def2afe633964b
Jeff Blue <517262600@qq.com>
2022-11-14 11:15:21 +0800

    add test for ZREM

--- memdb/sorted_set_test.go
+++ memdb/sorted_set_test.go
@@ -5,18 +5,18 @@ import (
 	"fmt"
 	"github.com/innovationb1ue/RedisGO/resp"
 	"strconv"
+	"strings"
 	"testing"
 )
 
-var m *MemDb
 var ctx context.Context
 
 func init() {
-	m = NewMemDb()
 	ctx = context.Background()
 }
 
-func TestZADD(t *testing.T) {
+func MakeTestDB() *MemDb {
+	m := NewMemDb()
 	zadd(ctx, m, MakeCommandBytes("zadd a 555 hero"), nil)
 	zadd(ctx, m, MakeCommandBytes("zadd a 333 ggbob"), nil)
 	zadd(ctx, m, MakeCommandBytes("zadd a 444 jeff"), nil)
@@ -26,11 +26,16 @@ func TestZADD(t *testing.T) {
 	zadd(ctx, m, MakeCommandBytes("zadd a 999 ggbob"), nil)
 	zadd(ctx, m, MakeCommandBytes("zadd a 888 a1"), nil)
 	// -999 444 555 666 888 999 with a total of 6
+	return m
+}
+
+func TestZADD(t *testing.T) {
+	MakeTestDB()
 }
 
 func TestZRange(t *testing.T) {
 	// need to make sure that we have put the correct values in it
-	TestZADD(t)
+	m := MakeTestDB()
 	// test zrange
 	res := zrange(ctx, m, MakeCommandBytes("zrange a 0 100 withscores"), nil)
 	resArr := res.(*resp.ArrayData)
@@ -54,5 +59,20 @@ func TestZRange(t *testing.T) {
 	if score, err := strconv.ParseFloat(resStrings[len(resStrings)-1], 64); err != nil || score != 999 {
 		t.Error("zrange score is wrong")
 	}
+}
 
+func TestZREM(t *testing.T) {
+	m := MakeTestDB()
+	zrem(ctx, m, MakeCommandBytes("zrem a ggbob"), nil)
+	res := zrange(ctx, m, MakeCommandBytes("zrange a 0 100 withscores"), nil)
+	if strings.Contains(string(res.ByteData()), "ggbob") {
+		t.Error("ZREM does not delete the key")
+		t.FailNow()
+	}
+	zrem(ctx, m, MakeCommandBytes("zrem a jeff"), nil)
+	res = zrange(ctx, m, MakeCommandBytes("zrange a 0 100 withscores"), nil)
+	if strings.Contains(string(res.ByteData()), "jeff") {
+		t.Error("ZREM does not delete the key")
+		t.FailNow()
+	}
 }
