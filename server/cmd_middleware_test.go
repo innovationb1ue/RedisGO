e762a089f978b2c89b2e6ef9029587406e688647
Jeff Blue <517262600@qq.com>
2023-01-22 16:59:37 +0800

    add test for filter collection delete operation

--- server/cmd_middleware_test.go
+++ server/cmd_middleware_test.go
@@ -26,5 +26,21 @@ func TestMiddleware_Add(t *testing.T) {
 	}
 	assert.Equal(t, 1, callCount)
 	assert.Equal(t, newCmd, memdb.MakeCommandBytes("should get passed"))
+}
 
+func TestDeleteMiddleware(t *testing.T) {
+	callCount := 0
+	simpleFilter := func(cmd [][]byte) ([][]byte, error) {
+		callCount += 1
+		return cmd, nil
+	}
+	middlewareInstance.Add(simpleFilter)
+	middlewareInstance.Delete(simpleFilter)
+	cmd := memdb.MakeCommandBytes("should get passed")
+	newCmd, err := middlewareInstance.Filter(cmd)
+	if err != nil {
+		t.Error("Delete logic is broken")
+	}
+	assert.Equal(t, 0, callCount, "filter function is not deleted after calling delete")
+	assert.Equal(t, newCmd, memdb.MakeCommandBytes("should get passed"))
 }
